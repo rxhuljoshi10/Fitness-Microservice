e6bdf387e810392c66eab2ea606c0f19aac662a8
Rahul Joshi <joshirahul7040@gmail.com>
2025-08-20 22:45:08 +0530

    RabbitMQ listener setup

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -0,0 +1,27 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.Repo.RecommendationsRepo;
+import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+//    private final ActivityAIService aiService;
+    @Autowired
+    private  RecommendationsRepo recommendationsRepo;
+
+    @RabbitListener(queues = "activity.queue")
+    public void processActivity(Activity activity) {
+        log.info("Received activity for processing: {}", activity.getId());
+//        Recommendation recommendation = aiService.generateRecommendation(activity);
+//        recommendationRepository.save(recommendation);
+    }
+}
